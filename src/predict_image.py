28273932ccaf0b40058d4bd9a13c461bd2e10ed4
Håkon Skirstad Grini <hkonsg@gmail.com>
2019-05-15 21:49:38 -0400

    predict_image cleanup
    Cleaned up code, and added some comments

--- src/predict_image.py
+++ src/predict_image.py
@@ -1,5 +1,6 @@
 from test_methods import *
 from unet import *
+import numpy as np
 
     
 def predict_relevant(image):
@@ -8,24 +9,15 @@ def predict_relevant(image):
     weights = 'weights.hdf5'
     #load premade weights
     model.load_weights(weights)
-    #we make sure to recreate shape of previous image
+    #we make sure to be able to recreate the shape of previous image
     prev_shape = image.shape
     #image must be reshaped to be predicted upon
     image = np.reshape(image,(1,)+image.shape)
     #get predicted array
-    result = model.predict(image)
-    print(result.mean())
-    #cv2.imshow("Image",result)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    #reshape back 
-    # save_result("camera_resized",result,"image%d.png")
-
+    result = model.predict(image)  
+    #must reshape result from an array of images to a single image
     result = np.reshape(result,(prev_shape[0],prev_shape[1],1))
     #find relevant pixels 
     pixels = get_pixel_list_of_obstacles(result)
     
     return pixels, np.reshape(result,(prev_shape[0],prev_shape[1]))
-
-
-
